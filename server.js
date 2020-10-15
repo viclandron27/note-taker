357c05ab79ce5e141334c6c1e33e187d7cff0507
Victoria Landron <viclandron@gmail.com>
2020-10-15 13:34:12 -0400

    successfully set up server

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+//parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+
+//parse incoming JSON data
+app.use(express.json());
+
+app.use(express.static('public'));
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
