efdd004a7fffc184031ca5c87a57f0975efa63b6
Victoria Landron <viclandron@gmail.com>
2020-10-18 16:37:42 -0400

    added code

--- routes/apiRoutes/notesRoute.js
+++ routes/apiRoutes/notesRoute.js
@@ -1,13 +1,21 @@
 const router = require('express').Router();
-const savedNotes = require('../../db/db.json');
+const  savedNotes  = require('../../db/db.json');
 
-router.get('/api/notes', (req, res) => {
+router.get('/notes', (req, res) => {
     console.log("GET:", savedNotes)
     let results = savedNotes;
-    if (req.query) {
-        results = filterByQuery(req.query, results);
-    }
+    // if (req.query) {
+    //     results = filterByQuery(req.query, results);
+    // }
     res.json(results);
 });
-
+router.post('/notes', (req, res) => {
+    console.log("POST:", req.body)
+    const savedNotes = saveNote(req.body)
+    res.json(savedNotes)
+    
+});
+router.delete('/notes/:id', (req, res) => {
+    console.log("DELETE:", req.params)
+});
 module.exports = router;
